eec5ed82178f8f376457741bda29472ed212bdc5
vdi-terminal <vdi-terminal@EDU-WSB.local>
2024-06-02 15:28:50 +0200

    Naprawiono problem z iportem modulu app w testach

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,11 @@
-from app import app
+import os
+import sys
 import pytest
+
+# Dodaj katalog główny projektu do ścieżki modułów
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import app
 
 @pytest.fixture
 def client():
